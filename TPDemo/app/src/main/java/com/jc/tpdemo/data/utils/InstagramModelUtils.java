f5fc719afdfd28a8a005d2494fcec9d500e9c83d
Jorge Calado <jorge.s.calado@gmail.com>
2015-04-12 14:51:23 +0100

    Date display in the instagram items
    * Now date is correctly displayed
    
    * Screen rotation locked

--- TPDemo/app/src/main/java/com/jc/tpdemo/data/utils/InstagramModelUtils.java
+++ TPDemo/app/src/main/java/com/jc/tpdemo/data/utils/InstagramModelUtils.java
@@ -3,6 +3,9 @@ package com.jc.tpdemo.data.utils;
 import com.jc.tpdemo.data.models.TagQueryResult;
 import com.jc.tpdemo.models.InstagramListItem;
 
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,9 +17,29 @@ public class InstagramModelUtils {
         ArrayList<InstagramListItem> items = new ArrayList<>();
 
         for(TagQueryResult.Media m : queryResult.data){
-            items.add(new InstagramListItem(m.getImageURL(), m.getUsername(), m.createTime));
+            String date = getDate(m.createTime);
+
+            items.add(new InstagramListItem(m.getImageURL(), m.getUsername(), date));
         }
 
         return items;
     }
+
+    /**
+     *
+     * @param timeStamp timestamp in seconds
+     * @return date representation
+     */
+    private static String getDate(String timeStamp){
+        long timestampNorm = Long.parseLong(timeStamp) * 1000;
+        try{
+            DateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+            Date netDate = (new Date(timestampNorm));
+            return sdf.format(netDate);
+        }
+        catch(Exception ex){
+            return "xx";
+        }
+    }
+
 }
